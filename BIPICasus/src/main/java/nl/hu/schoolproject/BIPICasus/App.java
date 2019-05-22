477ea4274427080ae1530d6905c4a8d2f100e852
RoordinkFrank <frank.roordink@live.nl>
2019-05-22 11:40:56 +0200

    Initial Commit

---
+++ BIPICasus/src/main/java/nl/hu/schoolproject/BIPICasus/App.java
@@ -0,0 +1,15 @@
+package nl.hu.schoolproject.BIPICasus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
